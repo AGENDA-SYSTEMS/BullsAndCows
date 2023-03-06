8d749521eaf394bd3969f2a24de96e06041f4ff8
AGENDA-SYSTEMS <hgendov@gmail.com>
2023-03-06 13:47:22 +0200

    v1 update

---
+++ BullsAndCows.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int player_one_numbers[4];
+    int player_two_numbers[4];
+
+printf("===================================\n");
+printf("====BULL AND COWS v1.01 by DxTR====\n");
+printf("===================================\n");
+printf("\n");
+for(int i=0;i<=3;i++){
+printf("====PLAYER ONE - ENTER NUMBER FROM 0 to 9====DIGIT:%d\n",i+1);
+scanf("%1d", &player_one_numbers[i]);
+// CHECK ZERO ELEMENT IS PASSED
+if(i>0){
+for(int j=i;j>0;j--){
+
+}
+
+}
+//ZERO ELEMENT CHECK LOGIC
+ }
+
+return 0;
+
+}
+
